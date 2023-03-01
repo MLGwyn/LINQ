6cf447699af8f66e2f062a0caa9b66bee04b9b4e
Melissa Gwyn <melissagwyn@gmail.com>
2023-02-28 19:46:15 -0500

    Initial intro to expressions

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQ
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Func<int, int> MultiplyBy2 = value => value * 2;
+            // int MultiplyBy2(int value)
+            // {
+            //     return value * 2;
+            // }
+            var scores = new List<int> { 42, 100, 98, 15 };
+            var newScores = new List<int>();
+            foreach (var score in scores)
+            {
+                var doubled = MultiplyBy2(score);
+                newScores.Add(doubled);
+            }
+
+            Console.ReadLine();
+
+        }
     }
-  }
 }
